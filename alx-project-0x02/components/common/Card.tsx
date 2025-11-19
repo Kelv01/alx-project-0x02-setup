76b655423e04ed3178d0a813ba49d5b1ff0c29f9
kelv01 <kelvinnjugunanungari@gmail.com>
2025-11-19 12:22:36 +0300

    added card component

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -3,7 +3,7 @@ import { CardProps } from "@/interfaces";
 
 const Card: React.FC<CardProps> = ({ title, content }) => {
   return (
-    <div className="max-w-7xl h-96 rounded-lg shadow-lg transition-transform hover:scale-105">
+    <div className="bg-amber-300 max-w-2xs p-4 m-4 h-96 rounded-lg shadow-lg transition-transform hover:scale-105">
       <h2 className="text-left text-2xl font-bold mb-2 text-gray-600">
         {title}
       </h2>
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,8 +1,12 @@
+import Card from '@/components/common/Card'
 import React from 'react'
 
 const home = () => {
   return (
-    <div><h1 className='text-center text-4xl p-4'>Welcome</h1></div>
+    <div><h1 className='text-center text-4xl p-4'>Welcome</h1>
+    <Card title='Project'
+    content='reusable card' />
+    </div>
   )
 }
 
